6f3c1f455512d666a7cae03a129042a2d72739df
TOUHAMI <rachidtouhami90@gmail.com>
2018-12-24 03:28:17 +0100

    add show/hide menu

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -77,6 +77,7 @@ class UserController extends AppController
                 ];
             }
         }
+        $data['hidemenu'] = !$this->isLogged();
         $data['messages'] = $messages;
         $this->render('user/login', $data);
     }
